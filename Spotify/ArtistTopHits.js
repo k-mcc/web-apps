11a5d36ee3677fc04d885a3c08341bc72ce3bdea
k-mcc <67031674+k-mcc@users.noreply.github.com>
2020-10-08 18:53:10 -0400

    Update ArtistTopHits.js to pull tracks from multiple top artists

--- Spotify/ArtistTopHits.js
+++ Spotify/ArtistTopHits.js
@@ -1,28 +1,5 @@
 var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("topArtists");
 
-function getArtistTopHits(id) {
-  
-  var sp = getService();
-  
-  var url = "https://api.spotify.com/v1/artists/" + id + "/top-tracks?market=ES";
-  
-  var response = refreshToken(sp, function() {
-      return UrlFetchApp.fetch(url, {
-        headers: {
-          Authorization: 'Bearer ' + sp.getAccessToken(),
-        }
-      });
-    });
-  var result = JSON.parse(response.getContentText());
-  
-  Logger.log(result);
-  
-  return result;
-}
-
-
-var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("topArtists");
-
 
 // returns a list of track IDs from the top artists
 // with field scores greater than or equal to the minScore for the 
@@ -31,20 +8,18 @@ var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("topArtists");
 // qualifier options: acousticness, danceability, energy, instrumentalness, 
 //                    liveness, loudness, speechiness, valence, & tempo
 function calculateBestPlaylist(qualifier, minScore) {
+  var row = 6; // "place" of the target artists.
   var column = 2;
   var hits = [];
   for (var i = 0; i < 3; i++) { // iterates 3 times, once for each of the time lengths.
-    var topArtistId = sheet.getRange(2, column).getValue();
+    var topArtistId = sheet.getRange(row, column).getValue();
     hits.push(getArtistTracks(topArtistId));
     column = column + 2;
   }
   var cell = sheet.getRange(sheet.getLastRow()+1, 1);
   
-  //let tracks = new Map();
-  // tracks.set('type','uri');
   
   var track1Types = getValues(hits[0], "type");
-  
   var track1Uris = getValues(hits[0], "uri");
   
   var topTracks = [];
@@ -52,6 +27,26 @@ function calculateBestPlaylist(qualifier, minScore) {
   for (var i = 0; i < track1Uris.length; i++) {
     if (  track1Types[i] == "track"  ) {
       var id = track1Uris[i].substring(14);
+      topTracks.push(id);
+    }
+  }
+  
+  var track2Types = getValues(hits[1], "type");
+  var track2Uris = getValues(hits[1], "uri");
+  
+  for (var i = 0; i < track2Uris.length; i++) {
+    if (  track2Types[i] == "track"  ) {
+      var id = track2Uris[i].substring(14);
+      topTracks.push(id);
+    }
+  }
+  
+  var track3Types = getValues(hits[2], "type");
+  var track3Uris = getValues(hits[2], "uri");
+  
+  for (var i = 0; i < track3Uris.length; i++) {
+    if (  track3Types[i] == "track"  ) {
+      var id = track3Uris[i].substring(14);
       topTracks.push(id);
     }
   }
@@ -66,25 +61,23 @@ function calculateBestPlaylist(qualifier, minScore) {
  * equal to 0.5 
  */
 function analyzeTrackList(tracks, qualifier, minScore) { // qualifier is a string of the audio feature to be analyzed.
-  var trackProperties = [];
-  let qualifierMap = new Map();
   var qualifyingTracks = [];
   
   for (var i = 0; i < tracks.length; i++) {
-    trackProperties.push(getTrackProperties(tracks[i]));
-    qualifierMap.set(tracks[i], getValues(trackProperties[i], qualifier));
-  }
-  
-  for (var i = 0; i < tracks.length; i++) {
-   
-    if (qualifierMap.get(tracks[i]) >= minScore) qualifyingTracks.push(tracks[i]);
-  
+    var properties = getTrackProperties(tracks[i]);
+    var qualifierValues = getValues(properties, qualifier);
+    Logger.log(qualifierValues[0]);
+    
+    if (qualifierValues[0] > minScore) {
+      Logger.log("madeit-->" + tracks[i]);
+      qualifyingTracks.push(tracks[i]);
+    }
+    Logger.log(tracks[i]);
   }
   
   return qualifyingTracks;
   
 }
-
 
 function getTrackProperties(id) {
   
@@ -100,8 +93,6 @@ function getTrackProperties(id) {
       });
     });
   var result = JSON.parse(response.getContentText());
-  
-  Logger.log(result);
   
   return result;
 }
@@ -125,3 +116,4 @@ function getArtistTracks(id) {
   
   return result;
 }
+

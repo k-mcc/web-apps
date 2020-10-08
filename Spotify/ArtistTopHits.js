7f1424ac22b84de6c749b8d407533ab74bf404a8
k-mcc <67031674+k-mcc@users.noreply.github.com>
2020-10-08 13:27:44 -0400

    Update ArtistTopHits.js

--- Spotify/ArtistTopHits.js
+++ Spotify/ArtistTopHits.js
@@ -1,74 +1,16 @@
 var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("topArtists");
 
-function getPopularArtistsHits() {
-  var column = 2;
-  
-  var hits = [];
-  
-  for (var i = 0; i < 3; i++) { // iterates 3 times, once for each of the time lengths.
-    var topArtistId = sheet.getRange(2, column).getValue();
-    
-    hits.push(getArtistTopHits(topArtistId));
-    
-    column = column + 2;
-  }
-  
-  var cell = sheet.getRange(sheet.getLastRow()+1, 1);
-  
-  var track1 = getObjects(hits[0], "type");
-  var track2 = getObjects(hits[1], "type");
-  var track3 = getObjects(hits[2], "type");
-  
-  var trackNames1 = getValues(track1, "uri");
-  var trackNames2 = getValues(track2, "uri");
-  var trackNames3 = getValues(track3, "uri");
-  
-  //TODO: remove all name values w the names of the artists. so that only track names remain.
-  
-  //TODO: identify songs and ids of those songs. Do we even need the names? I don't think so, that's backend stuff! You just need to be
-  //      able to upload the songs to the new playlist by analyzing and leveraging specific fields of the existing track object!
-  
-  //TODO: upload randomly or algorithmically (eventually) selected songs to a new playlist by matching their ids.
-  
-  /*var x = 0;
-  
-  
-  //TODO: debug this...
-  
-  
-  
-  for (var i = 0; i < trackNames1.length; i++) {
-    if ((""+trackNames1).includes("track")) {
-      var id = (""+trackNames1).substring(14);
-    }
-    cell.offset(i+1, x).setValue(id);
-  }
-  x++;
-  
-  for (var i = 0; i < trackNames2.length; i++) {
-    cell.offset(i+1, x).setValue(trackNames2[i]);
-  }
-  x++;
-  
-  for (var i = 0; i < trackNames3.length; i++) {
-    cell.offset(i+1, x).setValue(trackNames3[i]);
-  }
-  x++;*/
-  
-  
-}
-
 function getArtistTopHits(id) {
   
-  var sf = getService();
+  var sp = getService();
   
   var url = "https://api.spotify.com/v1/artists/" + id + "/top-tracks?market=ES";
   // related artists--> https://api.spotify.com/v1/artists/{id}/related-artists
   
-  var response = refreshToken(sf, function() {
+  var response = refreshToken(sp, function() {
       return UrlFetchApp.fetch(url, {
         headers: {
-          Authorization: 'Bearer ' + sf.getAccessToken(),
+          Authorization: 'Bearer ' + sp.getAccessToken(),
         }
       });
     });

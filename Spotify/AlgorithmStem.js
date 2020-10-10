18f085d3a6fb8704e8271d5b6fbd82d7e9dc0456
k-mcc <67031674+k-mcc@users.noreply.github.com>
2020-10-10 16:14:07 -0400

    Update AlgorithmStem.js

--- Spotify/AlgorithmStem.js
+++ Spotify/AlgorithmStem.js
@@ -59,33 +59,6 @@ function getUserID() {
   return id[0];
 }
 
-/* Creates and fills a Kid-Friendly playlist through the Kid-Friendly algorithm.
- * Makes new playlist and fills it with kid-friendly tracks from the most
- * popular tracks of the user's top artists within the past 4 weeks.
- */
-function createKidFriendlyPlaylist() {
-  var requirements = [];
-  var valence = [];
-  valence.push("valence");
-  valence.push(0.6);
-  valence.push(">=");
-  requirements.push(valence);
-  var energy = [];
-  energy.push("energy");
-  energy.push(0.6);
-  energy.push(">=");
-  requirements.push(energy);
-
-  var httpResponse = createPlaylist("Kid-Friendly Playlist","Kid-friendly top hits from your latest favorite artists, so that everyone in the room can enjoy the music.");
-  Logger.log(httpResponse);
-  var uris = getValues(httpResponse, "uri");
-  var playlistId = "";
-  for (var i = 0; i < uris.length; i++) {
-    if (uris[i].includes("playlist")) playlistId = (uris[i]).substring(17);
-  }
-  fillPlaylist(playlistId, requirements, true);
-}
-
 /* @param playlistId      id of the target playlist
  * @param requirements    array of requirements for tracks to be added
  * @param clean           false if added tracks can be marked as "explicit"
